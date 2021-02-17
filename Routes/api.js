a6240236c7565c032d82f129931a76de268c97e2
Jaycie McBrayer <73480703+jayciemcbrayer@users.noreply.github.com>
2021-02-17 15:03:41 -0600

    Auth0
    Added Auth0 information to api.js

--- Routes/api.js
+++ Routes/api.js
@@ -2,6 +2,26 @@ const express = require ('express');
 const router = express.Router();
 const Todo = require('../models/todo');
 
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'MkW6Qq0ZL8ybQkX5vAUOyxAC3sZAHlY9',
+  issuerBaseURL: 'https://dev-7a9gie-o.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 router.get('/todos', (req, res, next) => {
 
   //this will return all the data, exposing only the id and action field to the client
